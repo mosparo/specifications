889ca37f427277ea9c3b51942e35aaf865f666c5
Matthias Zobrist <matthias.zobrist@zepi.net>
2022-10-09 10:53:38 +0200

    Initial commit

---
+++ src/Exception.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Exception for the mosparo specifications
+ *
+ * @author Matthias Zobrist <matthias.zobrist@zepi.net>
+ * @copyright 2021-2022 mosparo
+ * @license MIT
+ */
+
+namespace Mosparo\Specifications;
+
+/**
+ * Exception for the mosparo specifications
+ *
+ * @author Matthias Zobrist <matthias.zobrist@zepi.net>
+ */
+class Exception extends \Exception
+{
+
+}
---
+++ src/Specifications.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * The Specification class helps to get access to the specifications.
+ *
+ * @author Matthias Zobrist <matthias.zobrist@zepi.net>
+ * @copyright 2021-2022 mosparo
+ * @license MIT
+ */
+
+namespace Mosparo\Specifications;
+
+/**
+ * The Specification class helps to get access to the specifications.
+ *
+ * @author Matthias Zobrist <matthias.zobrist@zepi.net>
+ */
+class Specifications
+{
+    const JSON_SCHEMA_RULE = 'rule.json';
+    const JSON_SCHEMA_RULESET = 'ruleset.json';
+
+    /**
+     * Returns the path to the specification resources
+     *
+     * @return string
+     */
+    public static function getSpecificationResourcesPath(): string
+    {
+        return __DIR__ . '/resources/';
+    }
+
+    /**
+     * REturns the path to the JSON schema
+     *
+     * @param $schema
+     * @return string
+     *
+     * @throws Exception
+     */
+    public static function getJsonSchemaPath($schema): string
+    {
+        $path = self::getSpecificationResourcesPath() . 'json-schema/' . $schema;
+        if (!file_exists($path)) {
+            throw new Exception(sprintf('Json schema "%s" not found.', $schema));
+        }
+
+        return $path;
+    }
+}
